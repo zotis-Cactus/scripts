ce716b0a8aa9040099d536926881e8bbe3ad0b61
Zotis Angelis <151250798+zotis-Cactus@users.noreply.github.com>
2024-03-05 11:53:17 +0200

    Update clear_cache.sh

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # chmod 755 filename (gia allagi dikeomaton)
-CACHE_DIR="/home/brigitte/www/image/cache/catalog"
+CACHE_DIR="/home/..../www/image/cache/catalog"
 
 if [ -d "$CACHE_DIR" ]; then
-    # Navigate to the cache directory
     cd "$CACHE_DIR" || exit
 
-    rm rm -rf *
+    rm -rf *
 
     echo "Cache in $CACHE_DIR have been cleared."
 else
